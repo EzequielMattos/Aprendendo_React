6a1e6c36a32383b86835e9856cad12c79d192f7c
Ezequiel Mattos <ezequielsfc@hotmail.com>
2024-01-09 00:15:50 -0300

    Criando o projeto

---
+++ pro-atividade-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          <li>Primeira Atividade</li>
+          <li>Segunda Atividade</li>
+          <li>Terceira Atividade</li>
+          <li>Quarta Atividade</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
