22bc55f3017427217a218e4d4d61b18fa1b51343
Ezequiel Mattos <ezequielsfc@hotmail.com>
2024-01-10 01:17:37 -0300

    Aprendendo um pouco sobre layout e como popular listas

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -1,19 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+let initialState = [
+  {
+    id: 1,
+    descricao: "Primeria Atividade"
+  },
+  {
+    id: 2,
+    descricao: "Segunda Atividade"
+  }
+]
+
 function App() {
+  const [atividades, setAtividades] = useState(initialState)
+
+  function addAtividade(e) {
+    e.preventDefault();
+
+    const atividade = {
+      id: document.getElementById('id').value,
+      descricao: document.getElementById('descricao').value
+    }
+    setAtividades([...atividades, {...atividade}]);
+    console.log(atividades);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          <li>Primeira Atividade</li>
-          <li>Segunda Atividade</li>
-          <li>Terceira Atividade</li>
-          <li>Quarta Atividade</li>
-        </ul>
-      </header>
+    <>
+    <form className="row g-3">
+      <div className="col-md-6">
+        <label for="inputEmail4" className="form-label">Id</label>
+        <input id="id" type="text" className="form-control"></input>
+      </div>
+      <div className="col-md-6">
+        <label for="inputEmail4" className="form-label">Descrição</label>
+        <input id="descricao" type="text" className="form-control"></input>
+      </div>
+      <hr />
+      <div class="col-12">
+        <button className='btn btn-outline-secondary' onClick={addAtividade}>+ Atividade</button>
+      </div>
+    </form>
+    <div className="mt-3">
+          {atividades.map(ativ => (
+            <div key={ativ.id} className="card mb-2 shadow-sm">
+            <div className="card-body">
+              <p className="card-text">{ativ.id} - {ativ.descricao}</p>
+            </div>
+          </div>
+          ))}          
     </div>
+    </>
   );
 }
 
