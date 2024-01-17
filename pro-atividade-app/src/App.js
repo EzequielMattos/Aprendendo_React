44d1e95cd962a4199dab943915593c7a9f8ef431
Ezequiel Mattos <ezequielsfc@hotmail.com>
2024-01-16 23:13:14 -0300

    melhorias no layout, função de deletar e editar

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -1,37 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import AtividadeForm from './components/AtividadeForm';
 import AtividadeLista from './components/AtividadeLista';
 
-let initialState = [
-  {
-    id: 1,
-    prioridade: '1',
-    titulo: "Título",
-    descricao: "Primeria Atividade"
-  },
-  {
-    id: 2,
-    prioridade: '2',
-    titulo: "Título",
-    descricao: "Segunda Atividade"
-  }
-]
-
 function App() {
-  const [atividades, setAtividades] = useState(initialState)
-  const [atividade, setAtividade] = useState([])
+  const [index, setIndex] = useState(0);
+  const [atividades, setAtividades] = useState([]);
+  const [atividade, setAtividade] = useState({id:0});
 
-  function addAtividade(e) {
-    e.preventDefault();
+  useEffect(() => {
+    atividades.length <= 0 ? setIndex(1) : setIndex(Math.max.apply(Math, atividades.map(item => item.id)) + 1)
+  }, [atividades])
 
-    const atividade = {
-      id: document.getElementById('id').value,
-      prioridade: document.getElementById('prioridade').value,
-      titulo: document.getElementById('titulo').value,
-      descricao: document.getElementById('descricao').value
-    }
-    setAtividades([...atividades, {...atividade}]);
+  function addAtividade(ativ) {
+    setAtividades([...atividades, {...ativ, id: index}]);
   }
 
   function deletarAtividade(id){
@@ -44,11 +26,21 @@ function App() {
     setAtividade(atividade[0])
   }
 
+  function atualizarAtividade(ativ) {
+    setAtividades(atividades.map(item => item.id === ativ.id ? ativ : item));
+    setAtividade({id: 0});
+  }
+
+  function cancelarAtividade() {
+    setAtividade({id: 0});
+  }
 
   return (
     <>
       <AtividadeForm
         addAtividade = {addAtividade}
+        atualizarAtividade = {atualizarAtividade}
+        cancelarAtividade = {cancelarAtividade}
         atividadeSelecionada = {atividade}
         atividades = {atividades}
       />
--- pro-atividade-app/src/components/AtividadeForm.js
+++ pro-atividade-app/src/components/AtividadeForm.js
@@ -1,38 +1,97 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+const atividadeInicial = {
+  id: 0,
+  titulo: '',
+  prioridade: 0,
+  descricao: ''
+}
 
 export default function AtividadeForm(props) {
 
+  const [atividade, setAtividade] = useState({atividadeAtual});
+
+  useEffect(() => {
+    if (props.atividadeSelecionada.id !== 0)
+      setAtividade(props.atividadeSelecionada);
+  }, [props.atividadeSelecionada])
+
     const inputTextHandler = (e) => {
-        const [name, value] = e.target;
+        const {name, value} = e.target;
+
+        setAtividade({...atividade, [name]: value});
+    };
+
+    function atividadeAtual(){
+      if (props.atividadeSelecionada.id !== 0){
+        return props.atividadeSelecionada;
+      }
+      else{
+        return atividadeInicial;
+      }
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if (props.atividadeSelecionada.id !== 0)
+        props.atualizarAtividade(atividade);
+      else
+        props.addAtividade(atividade);      
+
+      setAtividade(atividadeInicial);
     }
 
+    const handleCancelar = (e) => {
+      e.preventDefault();
+
+      props.cancelarAtividade();
+
+      setAtividade(atividadeInicial);
+    }
+   
   return (
-    <form className="row g-3">
+    <>
+    <h1>Atividade {atividade.id !== 0 ? atividade.id : ''} </h1>
+    <form className="row g-3" onSubmit={handleSubmit}>
       <div className="col-md-6">
-        <label className="form-label">Id</label>
-        <input id="id" name='id' type="text" className="form-control" onChange={inputTextHandler} value={Math.max.apply(Math, props.atividades.map(item => item.id)) + 1}></input>
+        <label className="form-label">Título</label>
+        <input id="titulo" name='titulo' type="text" className="form-control" value={atividade.titulo} onChange={inputTextHandler}></input>
       </div>
       <div className="col-md-6">
         <label className="form-label">Prioridade</label>
-        <select id="prioridade" className="form-select">
+        <select id="prioridade" name='prioridade' className="form-select" value={atividade.prioridade} onChange={inputTextHandler}>
           <option defaultValue="0">Selecione</option>
           <option value="1">Baixa</option>
           <option value="2">Normal</option>
           <option value="3">Alta</option>
         </select>
-      </div>
-      <div className="col-md-6">
-        <label className="form-label">Título</label>
-        <input id="titulo" type="text" className="form-control"></input>
-      </div>
-      <div className="col-md-6">
+      </div>     
+      <div className="col-md-12">
         <label className="form-label">Descrição</label>
-        <input id="descricao" type="text" className="form-control"></input>
-      </div>
+        <textarea id="descricao" name='descricao' type="text" className="form-control" value={atividade.descricao} onChange={inputTextHandler}></textarea>
       <hr />
-      <div className="col-12">
-        <button className='btn btn-outline-secondary' onClick={props.addAtividade}>+ Atividade</button>
+      </div>
+      <div className="col-12 mt-0">
+        {
+          atividade.id === 0 ? (
+          <button className='btn btn-outline-secondary' type='submit'>
+            <i className='me-2'>{<FontAwesomeIcon icon={faPlus}/>}</i>Atividade
+          </button>
+          ) : (
+          <>
+          <button className='btn btn-outline-success me-2' type="submit">
+            <i className='me-2'>{<FontAwesomeIcon icon={faPlus}/>}</i>Salvar
+          </button>
+          <button className='btn btn-outline-danger' onClick={handleCancelar}>
+            <i className='me-2'>{<FontAwesomeIcon icon={faPlus}/>}</i>Cancelar
+          </button>
+          </>
+        )}
       </div>
     </form>
-  )
+    </>
+  );
 }
