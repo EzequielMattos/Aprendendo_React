debe34884811f9d0a6811452a9772637ea38c543
Ezequiel Mattos <ezequielsfc@hotmail.com>
2024-02-29 13:57:55 -0300

    Iniciando a API com o backend

---
+++ back/ProAtividade_API/Controllers/AtividadeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProAtividade_API.Models;
+
+namespace ProAtividade_API.Controllers
+{
+    [ApiController]
+    public class AtividadeController : ControllerBase
+    {
+        [HttpGet("v1/atividade")]
+        public Atividade Get()
+        {
+            return new Atividade();
+        }
+
+        [HttpGet("v1/atividade/{id}")]
+        public string Get([FromRoute] int id)
+        {
+            return $"Meu primeiro metodo Get com parametro {id}";
+        }
+
+        [HttpPost("v1/atividade")]
+        public Atividade Post([FromBody] Atividade atividade)
+        {
+            return atividade;
+        }
+
+        [HttpPut("v1/atividade/{id}")]
+        public string Put([FromRoute] int id)
+        {
+            return "Meu primeiro metodo Put";
+        }
+
+        [HttpDelete("v1/atividade/{id}")]
+        public string Delete([FromRoute] int id)
+        {
+            return "Meu primeiro metodo Delete";
+        }
+    }
+}
